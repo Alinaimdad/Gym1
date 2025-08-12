811bb87832573a23043ef8577975d21065c7b8b8
ALINA <alinaimdad18@gmail.com>
2025-08-12 12:49:33 +0500

    Add PaymentPage component

---
+++ src/features/Payments/pages/PaymentPage.jsx
@@ -0,0 +1,13 @@
+// src/features/Payments/pages/PaymentPage.jsx
+import React from "react";
+
+const PaymentPage = () => {
+  return (
+    <div>
+      <h1>Payment Page</h1>
+      <p>Yahan payment form ya details dikhao.</p>
+    </div>
+  );
+};
+
+export default PaymentPage;
